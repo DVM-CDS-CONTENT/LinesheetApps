63be7e0d3c88f20a88b69d2a318c16fe95e51806
Sasikanesm <musasikan@central.co.th>
2023-05-30 13:24:40 +0700

    update_codevalidatepython2

--- my-app/src/page/linesheet/convertor/f_validate.py
+++ my-app/src/page/linesheet/convertor/f_validate.py
@@ -9,22 +9,40 @@ def open_file(path_name):
     sys.stdout.reconfigure(encoding='utf-8')
     df = pd.read_excel(abs_path, skiprows=11)
     actual_sku_count = len(df)
-    print(df.head())
+    # print(df.head())
     validate_number(actual_sku_count)
-    validate_character(df)
+    validate_character_th(df)
 
 def validate_number(actual_sku_count):
-    print(actual_sku_count)
+    # print(actual_sku_count)
     if actual_sku_count == expected_sku_count:
-        print("Validation passed: Number of SKUs matches the expected count.")
+        result_sku = "validate number of sku equal with count of sku in Linesheet."
     else:
-        print("Validation failed: Number of SKUs does not match the expected count.")
+        result_sku = "validate number of sku not equal with count of sku in Linesheet."
+    print(result_sku)
 
-def validate_character(df):
+def validate_character_th(df):
+    count_correct = 0
+    count_notcorrect = 0
+    product_name_th_error = []
     for index, row in df.iterrows():
-        product_name_th = row['ชื่อสินค้า ภาษาไทย Product Name TH']
-        if len(str(product_name_th)) > 100:
-            print(f"Validation failed: Product Name TH exceeds 100 characters in row {index+1}: {product_name_th}")
+        product_name_th = row['ชื่อสินค้า\nProduct Name [Thai]']
+        try:
+            if len(product_name_th) < 20:
+                # print(len(product_name_th))
+                count_correct = count_correct+1       
+            else:
+                count_notcorrect = count_notcorrect + 1
+                product_name_th_error.append(product_name_th)
+                # print(len(product_name_th),product_name_th)
+                
+        except TypeError:
+        # Handle the case where product_name_th is not a string
+            print("Invalid value for product name")
+    print(count_correct)
+    print(count_notcorrect)       
+    print(product_name_th_error)       
+        
 
 def validate_size(df):
     pass
