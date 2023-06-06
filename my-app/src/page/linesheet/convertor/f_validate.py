d2d0e9c7fd1e2806356b5c05ec2a830ceea5c595
Sasikanesm <musasikan@central.co.th>
2023-06-06 17:39:08 +0700

    Update code

--- my-app/src/page/linesheet/convertor/f_validate.py
+++ my-app/src/page/linesheet/convertor/f_validate.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
-import sys
 
-
+path_name = 'my-app/linesheet/CDS_LINESHEET_SANRIO_30_SKUs_TID_20230529180252220.xlsm'
 expected_sku_count = 30
+expected_types = ['.xlsm','.xlsx']
 
 
 def open_file(path_name):
@@ -11,41 +11,79 @@ def open_file(path_name):
     sys.stdout.reconfigure(encoding='utf-8')
     df = pd.read_excel(abs_path, skiprows=11)
     actual_sku_count = len(df)
-    print(df.to_string())
-
+    # print(df.head())
     validate_number(actual_sku_count)
     validate_character_th(df)
+    validate_type(path_name, expected_types)
 
 def validate_number(actual_sku_count):
     # print(actual_sku_count)
     if actual_sku_count == expected_sku_count:
-        result_sku = "validate number of sku equal with count of sku in Linesheet."
+        print("Validation passed: Number of SKUs matches the expected count.")
     else:
-        result_sku = "validate number of sku not equal with count of sku in Linesheet."
-    print(result_sku)
+        print("Validation failed: Number of SKUs does not match the expected count.")
 
 def validate_character_th(df):
     count_correct = 0
     count_notcorrect = 0
     product_name_th_error = []
-    for index, row in df.iterrows():
+    
+    for _, row in df.iterrows():
         product_name_th = row['ชื่อสินค้า\nProduct Name [Thai]']
+        ibc_no = row['IBC No.']
         try:
             if len(product_name_th) < 20:
-                # print(len(product_name_th))
-                count_correct = count_correct+1
+                count_correct += 1
             else:
-                count_notcorrect = count_notcorrect + 1
-                product_name_th_error.append(product_name_th)
-                # print(len(product_name_th),product_name_th)
-
+                count_notcorrect += 1
+                product_name_th_error.append((str(ibc_no),product_name_th))
         except TypeError:
-        # Handle the case where product_name_th is not a string
-            print("Invalid value for product name")
-    print(count_correct)
-    print(count_notcorrect)
-    print(product_name_th_error)
+            pass
+    
+    print(f"Correct count: {count_correct}")
+    print(f"Not correct count: {count_notcorrect}")
 
+    if len(product_name_th_error) > 0:
+        print("CDS with characters exceeding 20:")
+        for _, name in product_name_th_error:
+            print(f"CDS{_}: {name}")
+    else:
+        print(True)
+    validate_character_en(df)
+
+def validate_character_en(df):
+    count_correct = 0
+    count_notcorrect = 0
+    product_name_en_error = []
+    
+    for _, row in df.iterrows():
+        product_name_en = row['ชื่อสินค้า\nProduct Name [English]']
+        ibc_no = row['IBC No.']
+        try:
+            if len(product_name_en) < 20:
+                count_correct += 1
+            else:
+                count_notcorrect += 1
+                product_name_en_error.append((str(ibc_no),product_name_en))
+        except TypeError:
+            pass
+    
+    print(f"Correct count: {count_correct}")
+    print(f"Not correct count: {count_notcorrect}")
+
+    if len(product_name_en_error) > 0:
+        print("CDS with characters exceeding 20:")
+        for _, name in product_name_en_error:
+            print(f"CDS{_}: {name}")
+    else:
+        print(True)
+
+def validate_type(file_path, expected_type):
+    file_extension = os.path.splitext(file_path)[1]
+    if file_extension.lower() in [ext.lower() for ext in expected_types]:
+        print("File type is valid.")
+    else:
+        print("File type is not valid.")
 
 def validate_size(df):
     pass
@@ -62,7 +100,6 @@ def validate_productname_th(df):
 def validate_category(df):
     pass
 
-if __name__ == 'main':
-
-    path_name = sys.argv[1]
+if __name__ == '__main__':
+    import sys
     open_file(path_name)
